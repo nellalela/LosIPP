da2ccf4e5194842e6059697698fd1b2497a552b7
nellalela <nella.rg25@gmail.com>
2018-02-12 01:34:37 -0600

    antes de mover la base de  datos

---
+++ Seguimiento-congreso/mexican_states.js
@@ -0,0 +1,169 @@
+ var states = {
+   "type": "FeatureCollection",
+    "features": [
+      {
+        "type":"Feature",
+        "geometry":{
+          "type":"MultiPolygon",
+          "coordinates":[[[[-100.122803,19.938042],[-100.124733,19.951385],[-100.13028,19.974163],[-100.140007,20.003609],[-100.170013,20.060555],[-100.18251,20.082222],[-100.226395,20.145554],[-100.242226,20.165136],[-100.280319,20.204506],[-100.324722,20.243053],[-100.449173,20.373608],[-100.455986,20.430693],[-100.456947,20.457497],[-100.463623,20.501942],[-100.478607,20.573887],[-100.49015,20.602221],[-100.509171,20.62833],[-100.538475,20.650137],[-100.566116,20.660831],[-100.592369,20.675692],[-100.598206,20.695971],[-100.485703,20.900969],[-100.455765,20.930414],[-100.413345,20.94458],[-100.383621,20.940554],[-100.160843,20.902222],[-100.117012,20.901108],[-100.095703,20.929024],[-100.058899,21.061665],[-100.011253,21.178331],[-99.985497,21.196941],[-99.944588,21.197983],[-99.905563,21.186108],[-99.825974,21.175552],[-99.738197,21.226944],[-99.723335,21.247358],[-99.756676,21.274998],[-99.775566,21.298887],[-99.782791,21.324165],[-99.788765,21.384304],[-99.790543,21.419178],[-99.754456,21.504026],[-99.743057,21.521664],[-99.722229,21.541248],[-99.696259,21.55361],[-99.665909,21.545553],[-99.648621,21.528053],[-99.624451,21.490276],[-99.579727,21.423885],[-99.515564,21.434719],[-99.476395,21.442776],[-99.454727,21.44833],[-99.401047,21.46986],[-99.38681,21.49972],[-99.377365,21.541248],[-99.364876,21.558191],[-99.234871,21.636805],[-99.203346,21.644165],[-99.093903,21.335831],[-99.083069,21.288055],[-99.043259,21.268322],[-99.040848,21.160553],[-99.212921,21.113609],[-99.244591,21.11722],[-99.294693,21.145481],[-99.360291,21.097775],[-99.363892,21.068607],[-99.346115,21.047499],[-99.388206,20.920553],[-99.427986,20.868122],[-99.467087,20.855831],[-99.494446,20.815552],[-99.517509,20.735554],[-99.518623,20.713888],[-99.51001,20.686525],[-99.495796,20.666117],[-99.585846,20.614998],[-99.684723,20.569721],[-99.721115,20.553608],[-99.819458,20.512497],[-99.818344,20.409164],[-99.824448,20.32333],[-99.829102,20.269997],[-99.842224,20.270832],[-99.869865,20.268471],[-99.916397,20.257221],[-99.946709,20.242983],[-99.946953,20.205275],[-99.94043,20.185415],[-99.946945,20.159164],[-99.962234,20.129997],[-99.977089,20.108192],[-100.001328,20.086317],[-100.026398,20.065277],[-100.082642,20.002218],[-100.107224,19.966389],[-100.122803,19.938042]]]
+        ]
+      },
+      "properties":{
+        "id": 22,
+        "estado": "Querétaro",
+        "PorMexico": [
+          {
+            "nombre": "luis  carlo martnx torres",
+            "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+            "tendencia": "PRI PAN PRD numeros mil",
+            "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+            "partido": "PAN",
+            "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pan-128x128.jpg",
+            "estado": "Querétaro"
+          },
+          {
+            "nombre": "tania mdnj jnfdfsksb dbhcjs",
+            "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+            "tendencia": "PRI PAN PRD numeros mil",
+            "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+            "partido": "PAN",
+            "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pan-128x128.jpg",
+            "estado": "Querétaro"
+          }],
+          "TodosporMexico": [
+            {
+              "nombre": "pedro jhbchdb",
+              "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+              "tendencia": "PRI PAN PRD numeros mil",
+              "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+              "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+              "partido": "PRI",
+              "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+              "estado": "Querétaro"
+  
+            },
+            {
+              "nombre": "pablo jndssjvn; jefnflw",
+              "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+              "tendencia": "PRI PAN PRD numeros mil",
+              "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+              "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+              "partido": "PRI",
+              "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+              "estado": "Querétaro"
+            }],
+
+            "JuntosHaremos": [
+              {
+                "nombre": "pepe jneej juna jejfb",
+                "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+                "tendencia": "PRI PAN PRD numeros mil",
+                "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+                "partido": "PRD",
+                "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+                "estado": "Querétaro"
+    
+              },
+              {
+                "nombre": "karen jfrjeiddi",
+                "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+                "tendencia": "PRI PAN PRD numeros mil",
+                "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+                "partido": "PRD",
+                "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+                "estado": "Querétaro"
+              }]
+
+        
+      }
+},
+{
+       "type":"Feature",
+        "geometry":{
+       "type":"MultiPolygon",
+        "coordinates":[[[[-99.284866,19.142437],[-99.284454,19.157915],[-99.292511,19.191666],[-99.304451,19.21361],[-99.320976,19.238333],[-99.331116,19.264442],[-99.342789,19.304998],[-99.345284,19.339581],[-99.332787,19.369997],[-99.297508,19.38847],[-99.2714,19.39208],[-99.228828,19.403261],[-99.142921,19.483816],[-99.090561,19.484165],[-99.03334,19.455692],[-98.967514,19.30611],[-98.960281,19.281109],[-98.94001,19.13604],[-98.946259,19.112219],[-98.963852,19.089027],[-98.992783,19.076942],[-99.065567,19.079441],[-99.125984,19.112358],[-99.24556,19.144581],[-99.267784,19.144999],[-99.284866,19.142437]]]
+         ]
+    },
+    "properties":{
+    "id": 9,
+    "estado": "Ciudad de México",
+    "PorMexico": [
+      {
+        "nombre": "luisa dhkosfdb",
+        "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+        "tendencia": "PRI PAN PRD numeros mil",
+        "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+        "partido": "PAN",
+        "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pan-128x128.jpg",
+        "estado": "Ciudad de México",
+
+      },
+      {
+        "nombre": "tania hfvvk;p jdoi",
+        "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+        "tendencia": "PRI PAN PRD numeros mil",
+        "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+        "partido": "PAN",
+        "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pan-128x128.jpg",
+        "estado": "Ciudad de México",
+      }],
+      "TodosporMexico": [
+        {
+          "nombre": "pedro ukjnisw jfnllw",
+          "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+          "tendencia": "PRI PAN PRD numeros mil",
+          "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+          "partido": "PRI",
+          "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+          "estado": "Ciudad de México",
+
+        },
+        {
+          "nombre": "pablo khfnoeif",
+          "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+          "tendencia": "PRI PAN PRD numeros mil",
+          "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+          "partido": "PRI",
+          "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+          "estado": "Ciudad de México",
+        }],
+
+        "JuntosHaremos": [
+          {
+            "nombre": "pepe kjehfneqoqilk",
+            "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+            "tendencia": "PRI PAN PRD numeros mil",
+            "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+            "partido": "PRD",
+            "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+            "estado": "Ciudad de México",
+
+          },
+          {
+            "nombre": "karenednveoiu sq ifve",
+            "foto": "https://coo5.tuvotacion.com/imagenes_unicas/cual-perrito-es-mas-tierno-450089.jpg",
+            "tendencia": "PRI PAN PRD numeros mil",
+            "trayectoria": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "comentarios": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proi",
+            "partido": "PRD",
+            "logo":"http://transparencia.congresomich.gob.mx/media/partidos/logos/logo-pri-128x128.jpg",
+            "estado": "Ciudad de México",
+          }]
+
+    
+  }
+ 
+
+        }
+      
+    ]
+}
